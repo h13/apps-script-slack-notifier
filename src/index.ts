66842dc7a7bfdff59a39ef62f4288b751c65d535
Hironori Takahashi <h13.webmail@gmail.com>
2026-03-14 18:23:06 +0900

    chore: clean up template for slack notifier project

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,1 @@
-import { getGreeting } from "./greeting.js";
-
-function doGet(): GoogleAppsScript.HTML.HtmlOutput {
-  return HtmlService.createHtmlOutputFromFile("app")
-    .setTitle("GAS Web App")
-    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
-}
-
-function getMessage(): string {
-  return getGreeting();
-}
+// GAS entry points will be added in Task 11
